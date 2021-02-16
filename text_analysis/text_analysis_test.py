08918b0248befcbf8f444d6c1c66b98c8f067ff0
ryan2214 <ryan2214@users.noreply.github.com>
2021-02-16 21:34:59 +0800

    Update text_analysis_test.py

--- text_analysis/text_analysis_test.py
+++ text_analysis/text_analysis_test.py
@@ -6,7 +6,7 @@ def test_read_json():
     with open("test.txt","w") as f:
         f.write("supercool")
 
-    fu = File_Uploader("test.json")
+    fu = File_Uploader("ta_test.json")
     i = fu.upload("test.txt")
-    data = read_json("test.json")
+    data = read_json("ta_test.json")
     assert len(data) > 0
