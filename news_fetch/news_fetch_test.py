372de8b7f8cf6121e7074401702d2eca634d049a
ryan2214 <ryan2214@users.noreply.github.com>
2021-02-16 21:05:13 +0800

    Update news_fetch_test.py

--- news_fetch/news_fetch_test.py
+++ news_fetch/news_fetch_test.py
@@ -6,3 +6,7 @@ def test_get_from_url():
 
     i = len(res)
     assert i > 0
+
+def test_store_list_to_json():
+    res = get_from_url("https://www.youtube.com/")
+    assert store_list_to_json(res,"web_test.json") == 0
