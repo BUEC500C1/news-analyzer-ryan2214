15781be2ee3ee7278fbf8382eb86f4453f8cacde
ryan2214 <ryan2214@users.noreply.github.com>
2021-02-16 21:23:02 +0800

    text analysis

--- text_analysis/text_analysis.py
+++ text_analysis/text_analysis.py
@@ -1,1 +1,7 @@
 # tianhel@bu.edu 2021
+import json
+
+def read_json(jsname):
+    with open(jsname, "r", encoding='utf-8') as f:
+        data = json.loads(f) #data contains list of content
+    return data;
--- text_analysis/text_analysis_test.py
+++ text_analysis/text_analysis_test.py
@@ -1,1 +1,11 @@
 # tianhel@bu.edu 2021
+from file_upload import *
+
+def test_read_json():
+    with open("test.txt","w") as f:
+        f.write("supercool")
+
+    fu = File_Uploader("test.json")
+    i = fu.upload("test.txt")
+    data = read_json("test.json")
+    assert len(data) > 0
