c5dabcb5259c09b4ff3afbfe820a52fa731cb3dc
ryan2214 <ryan2214@users.noreply.github.com>
2021-02-16 21:55:44 +0800

    Update text_analysis.py

--- text_analysis/text_analysis.py
+++ text_analysis/text_analysis.py
@@ -5,4 +5,12 @@ def read_json(jsname):
     with open(jsname, "r", encoding='utf-8') as f:
         #data = json.loads(f)
         #data contains list of content
-        return f
+        data = []
+
+        for line in f.readlines():
+            line = line.strip()   # 使用strip函数去除空行
+            if len(line) != 0:
+                json_data = json.loads(line)
+                data.append(json_data)
+
+        return data
