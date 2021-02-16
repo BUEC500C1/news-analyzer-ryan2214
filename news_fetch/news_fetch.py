49c78070bd32c9bb52c47cd5576dbb2726963526
ryan2214 <ryan2214@users.noreply.github.com>
2021-02-16 20:25:07 +0800

    Update news_fetch.py

--- news_fetch/news_fetch.py
+++ news_fetch/news_fetch.py
@@ -4,21 +4,21 @@ import requests
 import lxml.html
 
 def get_from_url(url):
-	html = requests.get(url).content
-	selector = lxml.html.fromstring(html)
-	res = selector.xpath("///a/text()")
-	res1 = selector.xpath("///a/@href")
+    html = requests.get(url).content
+    selector = lxml.html.fromstring(html)
+    res = selector.xpath("///a/text()")
+    res1 = selector.xpath("///a/@href")
 
-	return res;
-	# print(res)
-	# print(res1)
- 
-	#for i,s in enumerate(res):
-	#    print(res1[i])
-	#    print(s)
+    return res;
+    # print(res)
+    # print(res1)
+
+    #for i,s in enumerate(res):
+    #    print(res1[i])
+    #    print(s)
 
 def store_list_to_json(res,jsname):
-	with open(jsname, "a", encoding='utf-8') as j:
+    with open(jsname, "a", encoding='utf-8') as j:
         j.write(data)
         print("json appended from list!")
         return 0
