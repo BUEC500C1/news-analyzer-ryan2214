057c67064d39d9dbcebb9aa9055080f081a93345
ryan2214 <ryan2214@users.noreply.github.com>
2021-02-16 19:12:50 +0800

    Update file_upload_test.py

--- file_upload/file_upload_test.py
+++ file_upload/file_upload_test.py
@@ -1,12 +1,15 @@
 # tianhel@bu.edu 2021
 from file_upload import *
+import os
 
 def test_success_file_upload():
     with open("test.txt","w") as f:
         f.write("supercool")
 
     fu = File_Uploader("test.json");
-    assert fu.upload("test.txt") == 0
+    i = fu.upload("test.txt")
+    os.remove("test.txt")
+    assert i == 0
 
 def test_fail_file_upload():
     fu = File_Uploader("test.json");
