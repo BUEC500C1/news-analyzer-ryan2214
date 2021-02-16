bb9c14f370a73d3bf1c1955cdf70ffc3959e2744
ryan2214 <ryan2214@users.noreply.github.com>
2021-02-16 21:47:44 +0800

    Update text_analysis.py

--- text_analysis/text_analysis.py
+++ text_analysis/text_analysis.py
@@ -3,5 +3,6 @@ import json
 
 def read_json(jsname):
     with open(jsname, "r", encoding='utf-8') as f:
-    #    data = json.loads(f) #data contains list of content
-    return f;
+        #data = json.loads(f)
+        #data contains list of content
+        return f
