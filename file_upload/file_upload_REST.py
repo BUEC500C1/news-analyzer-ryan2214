de6f0cf70a699f51782ebab25b6a71f317814d3f
ryan2214 <ryan2214@users.noreply.github.com>
2021-03-01 07:07:08 +0800

    Update file_upload_REST.py

--- file_upload/file_upload_REST.py
+++ file_upload/file_upload_REST.py
@@ -1,14 +1,44 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+from pathlib import Path
+import json
+import logging
 
 app = Flask(__name__)
 api = Api(app)
 
+##FILES = {
+##    'file1': {'file_name': 'cool news'},
+##    'file2': {'file_name': '?????'},
+##    'file3': {'file_name': 'super cool news!'},
+##}
+
 FILES = {
-    'file1': {'file_name': 'cool news'},
-    'file2': {'file_name': '?????'},
-    'file3': {'file_name': 'super cool news!'},
+    'files/file1': {
+                      'file_name': 'cool news',
+                      'text':'a cool story.'
+    }
 }
+
+def refresh_files_with_local_json(root_dir):
+    FILES = {}
+
+    path = Path(root_dir)
+    all_json_file = list(path.glob('*.json'))
+
+    for json_file in all_json_file:
+        file_name = json_file.parent.stem
+        with json_file.open() as f:
+            json_result = json.load(f)
+
+        file_obj = {file_name:json_result}
+        FILES.append(file_obj)
+
+def write_result_in_file(write_path , write_content):
+
+    with open(write_path,'w') as f:
+        json.dump(write_content,f)
+    logging.debug('writing file conpleted')
 
 
 def abort_if_file_doesnt_exist(file_id):
@@ -17,7 +47,7 @@ def abort_if_file_doesnt_exist(file_id):
 
 parser = reqparse.RequestParser()
 parser.add_argument('file_name')
-
+parser.add_argument('text')
 
 # File
 # shows a single file item and lets you delete a file item
@@ -46,9 +76,12 @@ class FileList(Resource):
 
     def post(self):
         args = parser.parse_args()
-        file_id = int(max(FILES.keys()).lstrip('file')) + 1
-        file_id = 'file%i' % file_id
-        FILES[file_id] = {'file_name': args['file_name']}
+        file_id = int(max(FILES.keys()).lstrip('files/file')) + 1
+        file_id = 'files/file%i' % file_id
+        FILES[file_id] = {
+                              'file_name': args['file_name'],
+                              'text': args['text']
+                          }
         return FILES[file_id], 201
 
 ##
@@ -56,7 +89,9 @@ class FileList(Resource):
 ##
 api.add_resource(FileList, '/files')
 api.add_resource(File, '/files/<file_id>')
-
+refresh_files_with_local_json(".")
 
 if __name__ == '__main__':
     app.run(debug=True)
+## host = 'XX.XX.XX.XX' ,port = 5000, debug = 'True'
+## accroding to EC2 public IP
