570850fe2a2462ea6731926c9c8189375d84f3bd
ryan2214 <ryan2214@users.noreply.github.com>
2021-03-22 20:34:55 +0800

    test nlp

--- text_analysis/text_analysis_test.py
+++ text_analysis/text_analysis_test.py
@@ -1,13 +1,5 @@
 # tianhel@bu.edu 2021
-from file_upload import *
-from text_analysis import *
+from senti_analyze import *
 
-def test_read_json():
-    with open("test.txt","w") as f:
-        f.write("supercool")
-
-    fu = File_Uploader("ta_test.json")
-    i = fu.upload("test.txt")
-
-    data = read_json("ta_test.json")
-    assert len(data) > 0
+def test_senti_analyze():
+    assert text_nlp("Good morning!\nHave a nice day!") > 0
