1fc3f20889a61fe2c5c1efb109cb250adf69f685
ryan2214 <ryan2214@users.noreply.github.com>
2021-03-22 23:06:09 +0800

    remove extra main

--- text_analysis/senti_analyze.py
+++ text_analysis/senti_analyze.py
@@ -12,9 +12,4 @@ def text_nlp(text):
     SENTI_RESULT = client.analyze_sentiment(request={'document': document}).document_sentiment.score
 
     return SENTI_RESULT
-
-if __name__ == '__main__':
-    path = u'List_of_Topics_for_MS_Project_and_Thesis.txt'
-    current_path = os.path.abspath('.')
-    contents = Path(current_path+'/'+path).read_text()
-    print(text_nlp(contents))
+    
